059602b750aaca625f2bbff8c5de1b6928825131
Ouroboros <ouroboros@users.noreply.github.com>
2026-03-19 18:12:25 +0000

    test: add sql_govsupport to smoke tests (query_govsupport tool)

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -43,6 +43,7 @@ TOOL_MODULES = [
     "ouroboros.tools.control",
     "ouroboros.tools.browser",
     "ouroboros.tools.review",
+    "ouroboros.tools.sql_govsupport",
 ]
 
 SUPERVISOR_MODULES = [
@@ -114,6 +115,8 @@ EXPECTED_TOOLS = [
     "compact_context",
     "list_available_tools",
     "enable_tools",
+    # SQL government support
+    "query_govsupport",
 ]
 
 
